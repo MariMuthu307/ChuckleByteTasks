e80285792cce57833cd95a9d6d111228b9e39dab
MariMuthu307 <marimuthuleo5@gmail.com>
2024-08-12 13:47:16 +0530

    Tasks

---
+++ ChuckleProjectTasks/TASKFOUR.js
@@ -0,0 +1,14 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Basic form validation (more can be added as needed)
+    if (name && email && message) {
+        alert('Thank you, ' + name + '! Your message has been sent.');
+        document.getElementById('contact-form').reset();
+    } else {
+        alert('Please fill in all fields.');
+    }
+}
---
+++ ChuckleProjectTasks/TASKTWO.js
@@ -0,0 +1,3 @@
+function buyNow() {
+    alert('Please Provide Your Details');
+}
